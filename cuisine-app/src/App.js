32acd0d617637193ff4d7f2ee873a5cf89eb0778
Chelsea-exe <cjackson145@student.gsu.edu>
2020-09-15 23:14:16 -0400

    added uuid npm package, added components, modified App.css and App.js

--- cuisine-app/src/App.js
+++ cuisine-app/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
 import './App.css';
 import Recipe from "./component/Recipe";
+import Alert from "./component/Alert";
 const App = () => {
     const [query, setQuery] = useState("");
     const [recipes, setRecipes] = useState([]);
+    const [alert, setAlert] = useState("");
 
     const APP_ID = "45a22c03";
 
@@ -13,12 +16,20 @@ const App = () => {
     const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
     const getData = async () => {
+        if(query !== "") {
         const result = await Axios.get(url);
+        if(!result.data.more) {
+            return setAlert("No food to match with name");
+        }
 
         setRecipes(result.data.hits)
 
         console.log(result);
+        setAlert("");
         setQuery("");
+        } else {
+            setAlert("Please fill the form")
+        }
     };
 
     const onChange = (e) => {
@@ -32,13 +43,15 @@ const App = () => {
 
     return (
         <div className="App">
-            <h1>Welcome to Food Searching App</h1>
+            <h1>daily recipes</h1>
+            <p></p>
             <form className="search-form" onSubmit={onSubmit}>
+                {alert !== "" && <Alert alert={alert} />}
                 <input type="text" placeholder="Search Recipes" autoComplete="off" onChange={onChange} value={query}/>
                 <input type="submit" value="search" />
             </form>
             <div className="recipes">
-                {recipes !== [] && recipes.map(recipe => <Recipe recipe={recipe} />)}
+                {recipes !== [] && recipes.map(recipe => <Recipe key={uuidv4 ()} recipe={recipe} />)}
 
             </div>
         </div>
